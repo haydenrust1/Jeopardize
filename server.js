2c8d540eb0ff2b5e8662064ca7a30cd6ac2d05a9
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-10-17 15:22:15 -0700

    fixing routes for server.js

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/questions-route")
+require("./routes/users-route")(app);
 
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
