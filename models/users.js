d29ff6c5fb725a004314d99cc018d1000e8b88e5
EG5924 <67492348+egilbertsen@users.noreply.github.com>
2020-10-17 14:59:47 -0700

    adding passport functions

--- models/users.js
+++ models/users.js
@@ -1,7 +1,24 @@
-module.exports = function (sequelize, DataTypes) {
-    let Users = sequelize.define('users', {
-        username: DataTypes.STRING
-    })
+var bcrypt = require("bcryptjs");
 
-    return Users;
-}
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+ 
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  return User;
+};
---
+++ routes/html-routes.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    if (req.user) {
+      res.redirect("/signedin");
+    }
+    res.sendFile(path.join(__dirname, "/registration"));
+  });
+
+  app.get("/login", function(req, res) {
+    if (req.user) {
+      res.redirect("/signedin");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/signedin", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signedin.html"));
+  });
+
+};
