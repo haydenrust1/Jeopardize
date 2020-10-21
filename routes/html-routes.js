8a3bdca9515fca6759ae4a05fd54d90dddc8679b
Haydenrust1 <haydenrust1@gmail.com>
2020-10-20 19:15:25 -0700

    fixes to schema, server, questions, and routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,23 +2,23 @@ var path = require("path");
 
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     if (req.user) {
       res.redirect("/signedin");
     }
     res.sendFile(path.join(__dirname, "/registration"));
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     if (req.user) {
       res.redirect("/signedin");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  app.get("/signedin", isAuthenticated, function(req, res) {
+  app.get("/signedin", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/signedin.html"));
   });
 
--- routes/questions-route.js
+++ routes/questions-route.js
@@ -1,7 +1,15 @@
 let db = require('../models');
 module.exports = function (app) {
+
     app.get("/api/questions", function (req, res) {
         db.Questions.findAll()
+            .then(function (result) {
+                res.json(result);
+            });
+    });
+
+    app.post("/api/questions", function (req, res) {
+        db.Questions.create(req.body)
             .then(function (result) {
                 res.json(result);
             });
